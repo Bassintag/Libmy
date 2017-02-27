00a734e762cebf1be4b16730ea3aedf376808872
Antoine <antoine.stempfer@epitech.eu>
2017-02-27 21:48:27 +0100

    Fixed bug

--- src/my_argparse/my_argparse_show_options.c
+++ src/my_argparse/my_argparse_show_options.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.stempfer@epitech.net>
 ** 
 ** Started on  Mon Feb 27 21:01:31 2017 Antoine Stempfer
-** Last update Mon Feb 27 21:34:21 2017 Antoine Stempfer
+** Last update Mon Feb 27 21:46:29 2017 Antoine Stempfer
 */
 
 #include <stdlib.h>
@@ -47,12 +47,12 @@ static int	my_argparse_calc_padding(t_arg_parser *parser)
   my_list_next(NULL);
   while ((arg = my_list_next(parser->optional)) != NULL)
     {
-      tmp = my_strlen(arg->identifier) + 3;
+      tmp = my_strlen(arg->identifier) + 1;
       if (arg->shorthand != NULL)
 	tmp += my_strlen(arg->shorthand) + 1;
       if (arg->nargs > 0)
 	tmp += 3;
-      tmp -= MAX(0, arg->nargs * 2 - 1);
+      tmp += MAX(0, arg->nargs * 2 - 1);
       if (tmp > res)
 	res = tmp;
     }
@@ -103,11 +103,21 @@ static int	my_argparse_append_option(t_arg *option, char **options,
 int	my_argparse_show_options(t_arg_parser *parser)
 {
   char	*options;
+  char	*tmp;
   t_arg	*option;
   int	padding;
+  int	len;
 
-  options = my_strdup("Options:\n   --help\n");
+  options = my_strcat("Options:\n   ", parser->help_id);
   padding = my_argparse_calc_padding(parser);
+  len = padding - my_strlen(parser->help_id);
+  tmp = malloc(sizeof(char) * len + 1);
+  tmp[len] = '\0';
+  while (--len >= 0)
+    tmp[len] = ' ';
+  options = my_strappend(options, tmp);
+  free(tmp);
+  options = my_strappend(options, "Display this help\n");
   my_list_next(NULL);
   while ((option = my_list_next(parser->optional)) != NULL)
     my_argparse_append_option(option, &options, padding);
